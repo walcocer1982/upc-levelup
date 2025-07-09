fb7f05cdb10ef200d431ffc195962ba255e11927
MICHAEL LIMACO <limaco.michael@gmail.com>
2025-07-09 00:54:18 -0500

    update forms

--- src/app/(dashboard)/founder/page.tsx
+++ src/app/(dashboard)/founder/page.tsx
@@ -2,7 +2,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Cabecera from "@/views/components/ui/Header";
+import Cabecera from "@/views/components/interface/Header";
 
 export default function FundadorPage() {
   const { status, data } = useSession();
